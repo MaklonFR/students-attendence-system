b930a82c319378327144d4fc97e2be057672a31b
MaklonFR <kallonjuve@gmail.com>
2026-02-26 22:12:19 +0800

    Update..

--- start-backend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Backend Server...
-echo Backend API: http://localhost:8000
-echo.
-cd backend\public
-php -S localhost:8000 router.php
--- start-frontend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Frontend Server...
-echo Frontend: http://localhost:3000
-echo.
-cd frontend
-php -S localhost:3000
